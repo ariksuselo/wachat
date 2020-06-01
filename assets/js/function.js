3e2d7a6fdfb9220e0758694fc242783b7f29c9df
Arik Sultan <57219459+ariksuselo@users.noreply.github.com>
2020-06-02 05:13:47 +0700

    Create function.js

---
+++ assets/js/function.js
@@ -0,0 +1,3 @@
+function isMobileWA()
+{return (/Android|webOS|iPhone|iPad|iPod|Windows Phone|IEMobile|Mobile|BlackBerry/i.test(navigator.userAgent) );}var elm = jQuery('a[href*="whatsapp.com"]');jQuery.each(elm, function(index, value){var item = jQuery(value).attr('href');if(item.indexOf('chat') != -1){//nothing
+} else if (item.indexOf('web.whatsapp') != -1 && isMobileWA()){var itemLink = item;var newLink = itemLink.replace('web.whatsapp', 'api.whatsapp');jQuery(value).attr("href", newLink);} else if (item.indexOf('api.whatsapp') != -1 && !isMobileWA()){var itemLink = item;var newLink = itemLink.replace('api.whatsapp', 'web.whatsapp');jQuery(value).attr("href", newLink);}});
